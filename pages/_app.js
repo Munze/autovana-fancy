c3ef8912480d73819f42c4a1dc5197ba1b62d923
Sasa <sasa.oravec@gmail.com>
2022-08-24 13:13:54 +0200

    searchSidebar outlines

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,22 @@
-import { BottomNav } from '../components/navigation/BottomNav'
-import { HeaderNavBar } from '../components/navigation/HeaderNavBar'
-import { TopHeader } from '../components/navigation/TopHeader'
-import { store } from '../store'
-import { Provider } from 'react-redux'
-import '../styles/globals.css'
+import { BottomNav } from "../components/navigation/BottomNav";
+import { HeaderNavBar } from "../components/navigation/HeaderNavBar";
+import { TopHeader } from "../components/navigation/TopHeader";
+import { store } from "../store";
+import { Provider } from "react-redux";
+import "../styles/globals.css";
+import { SearchSidebar } from "../components/navigation/SearchSidebar";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <>
-  <Provider store={store}>
-  <TopHeader />
-  <HeaderNavBar />
-  <Component {...pageProps} />
-  <BottomNav />
-  </Provider>
-  </>)
+    <>
+      <Provider store={store}>
+        <TopHeader />
+        <HeaderNavBar />
+        <Component {...pageProps} />
+        <BottomNav />
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
