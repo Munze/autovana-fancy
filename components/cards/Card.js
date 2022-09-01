953ae4a9bc584082581b51bbe5a35b4b2a30fcfa
Sasa <sasa.oravec@gmail.com>
2022-09-01 14:43:32 +0200

    card and browser color

--- components/cards/Card.js
+++ components/cards/Card.js
@@ -8,10 +8,10 @@ export const Card = () => {
     // <Link
     // href="/oglasi/1232"
     // >
-      <a href="/oglasi/123">
-        <div className="m-1 p-1 rounded-md shadow-md hover:shadow-green-400 hover:shadow-md bg-white  cursor-pointer">
-          <div className="container bg-slate-700 ">
-            <div className="shadow-sm ">
+      <Link href="/oglasi/123">
+        <div className="m-1 rounded-lg shadow-md hover:shadow-green-400 hover:shadow-md bg-white  cursor-pointer">
+          <div className="container">
+            <div className="shadow-sm">
               <Image
                 src="/img/6.webp"
                 width="100%"
@@ -21,20 +21,21 @@ export const Card = () => {
               />
             </div>
           </div>
-          <div className="">
-            <h3 className="text-md font-semibold text-blue-600 line-clamp-1 tracking-tight ">
+          <div className="my-2 mx-1">
+            <h3 className="text-sm font-semibold text-black line-clamp-1 tracking-tight ">
               Toyota Auris 1,8 HSD 5-TG. EDITION45
             </h3>{" "}
           </div>
-          <div className="flex flex-wrap space-x-1 justify-between mx-2">
+          <div className="flex flex-wrap space-x-1 justify-items-end mx-2">
             <PillSpec spec="2014" />
             <PillSpec spec="123.450 km" />
           </div>
+          <hr className="mx-2 my-2"/>
           <div className="grid grid-cols-1 justify-items-end">
-            <div className="font-bold text-xl pt-3 pr-3 proportional-nums">2.400 &euro; </div>
+            <div className="font-bold text-2xl py-1 pr-3 proportional-nums">2.400 &euro; </div>
           </div>
         </div>
-      </a>
+      </Link>
     // </Link>
   );
 };
--- components/cards/PillSpec.js
+++ components/cards/PillSpec.js
@@ -3,7 +3,7 @@ import React from "react";
 export const PillSpec = ({spec}) => {
   return (
     <div>
-      <span className="text-xs inline-block py-1 px-1.5 leading-none text-center whitespace-nowrap align-baseline font-semibold bg-green-100 text-green-800 border border-1 border-blue-800 rounded-md">
+      <span className="text-xs inline-block py-1 px-1.5 leading-none text-center whitespace-nowrap align-baseline font-semibold bg-blue-50 text-black border-dotted border-2 border-blue-800 rounded-md">
         {spec}
       </span>
     </div>
