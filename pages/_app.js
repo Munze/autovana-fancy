8d534d1b6c34de16c54d50c3961c6623e592d0aa
Sasa <sasa.oravec@gmail.com>
2022-09-01 17:33:10 +0200

    small change on _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,24 @@
 import { BottomNav } from "../components/navigation/BottomNav";
 import { HeaderNavBar } from "../components/navigation/HeaderNavBar";
-import { SessionProvider } from "next-auth/react"
-// import { TopHeader } from "../components/navigation/TopHeader";
+import { SessionProvider } from "next-auth/react";
 import { store } from "../store";
 import { Provider } from "react-redux";
 import "../styles/globals.css";
-// import { SearchSidebar } from "../components/navigation/SearchSidebar";
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
-    <SessionProvider session={session}>
-      <Provider store={store}>
-        <div className="md:fixed md:top-0 md:left-0 z-30 w-screen">
-          {/* <TopHeader /> */}
-          <HeaderNavBar />
-        </div>
-        <div className="bg-slate-200 md:mt-14">
-          <Component {...pageProps} />
-        </div>
-        <BottomNav />
-      </Provider>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <div className="md:fixed md:top-0 md:left-0 z-30 w-screen">
+            {/* <TopHeader /> */}
+            <HeaderNavBar />
+          </div>
+          <div className="bg-slate-200 md:mt-14">
+            <Component {...pageProps} />
+          </div>
+          <BottomNav />
+        </Provider>
       </SessionProvider>
     </>
   );
